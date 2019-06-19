b2c7d08d931deabb821ea8121b582099aea69618
pactera <pactera@dengfeng1>
2019-06-19 19:07:45 +0800

    配置中心

--- electronic-shop-pay/src/main/java/com/shop/electronic/PayServerApplication.java
+++ electronic-shop-pay/src/main/java/com/shop/electronic/PayServerApplication.java
@@ -1,17 +1,29 @@
 package com.shop.electronic;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-@MapperScan(basePackages="com.shop.electronic.pay.mapper")
+@MapperScan(basePackages = "com.shop.electronic.pay.mapper")
+@RestController
 public class PayServerApplication {
 
-	 public static void main(String[] args) {
-		 SpringApplication.run(PayServerApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(PayServerApplication.class, args);
 	}
-	
+
+	@Value("${info.name}")
+	String name;
+
+	@RequestMapping(value = "/hi")
+	public String hi() {
+		return name;
+	}
+
 }
